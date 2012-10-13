2f9b1e644bc5d41a2d530e76ec32b57ed7977632
Drogunov Igor <drogunov.igor@gmail.com>
2012-10-13 21:33:14 +0400

    Add simple type's description.

---
+++ modules/ndTypes.hs
@@ -0,0 +1,27 @@
+{-
+ - Module : ndTypes.hs
+ - Description : Модуль, описывающий типы данных, используемые для манипулирования на стеке
+ - Stability : experimental
+ -}
+
+--------------------------------------------------------------------------------
+{- Описание типов нашего языка -}
+--------------------------------------------------------------------------------
+data NDTYPE = NDTYPEi Int
+	|NDTYPEd Double
+	|NDTYPEc Char
+	|NDTYPEs String
+	|NDTYPEb Bool
+--------------------------------------------------------------------------------
+
+{-
+ - Usage example...
+main::IO ()
+
+main = print (func (NDTYPEi 10)) >> print (func (NDTYPEd 10.0))
+
+func::NDTYPE -> Int
+
+func (NDTYPEi a) = a::Int
+func (NDTYPEd a) = 100
+-}
