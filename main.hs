49452bfb3cac51f5de32db794b7e708fcc88f798
Drogunov Igor <drogunov.igor@gmail.com>
2012-11-13 00:15:56 +0400

    Вернулись к haskeline. Примитивная каменная загрузка пашет. радуемся и пинаем саню теперь.

--- main.hs
+++ main.hs
@@ -6,8 +6,9 @@ import NDParse
 import NDAction
 import NDActionHandlers
 import Text.Printf 
-import Data.Map
 import System.Console.Haskeline
+import Data.Map
+import Network.CGI
 
 main::IO ()
 
@@ -22,16 +23,18 @@ main = runInputT defaultSettings $loop Program{stack = [], funcs = fromList []}
 				Just "load" ->	getInputLine "9.:loading " >>=  
 								(\(Just t) -> return (words t)) >>=
 								(\t -> return $load prog t ) >>=
+								(\t -> liftIO t) >>=
 								(\t -> (outputStrLn $show (stack t)) >> loop t )
 				Just input ->	return (execute (parser input) prog) >>= 
 					 			(\t -> (outputStrLn $show (stack t)) >> loop t )
-{-
+
 		load::Program -> [String] -> IO Program
 		load prog [] = return prog
 		load prog (x:xs) =	readFile x >>=
 							(\t -> return $execute (parser t) prog) >>=
 							(\t -> load t xs)
--}
+{-
 		load::Program -> [String] -> Program
 		load prog [] = prog
 		load prog (x:xs) = load (execute (fparser x) prog) xs
+-}
