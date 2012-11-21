d2aefdd4c878648e202cedef4e5f31e4e0138fe8
dvdalex <dvdalex@nm.ru>
2012-11-22 01:14:08 +0400

    Причесал загрузку файлов

--- main.hs
+++ main.hs
@@ -10,36 +10,43 @@ import System.Console.Haskeline
 import Data.Map
 import Network.CGI
 
+import Prelude hiding (catch)
+import System.Directory
+import Control.Exception hiding (catch,throwIO)
+import System.IO.Error hiding (catch)
+
 main::IO ()
 
-main = runInputT defaultSettings $loop Program{stack = [], funcs = fromList []}
+main = runInputT defaultSettings $ loop Program{stack = [], funcs = fromList []}
 	where
 		loop::Program -> InputT IO()
 		loop Program{stack = ((NDTYPErr err):xs), funcs = f } = outputStrLn err >> 
 			loop Program{stack = xs, funcs = f}
-		loop prog  = do
+		loop prog = do
 			outputStrLn $ showNew (stack prog)
 			input <- getInputLine "9.: "
 			case input of
 				Nothing -> return ()
 				Just "q" ->	outputStrLn "9.: Good bye!!!!"
-				Just ('l':' ':files) ->	outputStrLn "9.:loading files" >>  
+				Just ('l':' ':files) ->	
+								outputStrLn ("9.: Loading files: " ++ files) >>  
 								return (words files) >>=
-								(\t -> return $ load prog t ) >>=
-								(\t -> liftIO t) >>=
+								(\t -> load prog t ) >>=
 								loop
 --								(\t -> (outputStrLn $ showNew (stack t)) >> loop t )
 				Just input ->	return (execute (parser input) prog) >>= 
 								loop
 --					 			(\t -> (outputStrLn $ showNew (stack t)) >> loop t )
 
-		load::Program -> [String] -> IO Program
 		load prog [] = return prog
-		load prog (x:xs) =	readFile x >>=
-							(\t -> return $execute (parser t) prog) >>=
+		load prog (x:xs) =	liftIO ( readFile x `catch` handleExistance )>>=
+							handleExistance' x prog >>=
 							(\t -> load t xs)
-{-
-		load::Program -> [String] -> Program
-		load prog [] = prog
-		load prog (x:xs) = load (execute (fparser x) prog) xs
--}
+					where 
+						  handleExistance err | isDoesNotExistError err = return ""
+											  | otherwise = throwIO err
+						  handleExistance' name prog [] = outputStrLn ("File \""++name++"\" is empty or doesn't exists") >> 
+														  return prog
+						  handleExistance' name prog file = outputStrLn ("Executing file \""++name++"\"") >>
+														    return (execute (parser file) prog)
+														    
