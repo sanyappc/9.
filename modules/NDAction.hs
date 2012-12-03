6600fed0ce0458e1296bec0a219cf56252782a4a
Drogunov Igor <drogunov.igor@gmail.com>
2012-12-04 00:46:59 +0400

    fix NDAction.

--- modules/NDAction.hs
+++ modules/NDAction.hs
@@ -14,33 +14,33 @@ data NDCoords = LinePos Integer Integer
 -------------------------------------------------------------------------------
 {- Тип Action предназначен для описания базовых функций нашего языка -}
 -------------------------------------------------------------------------------
-data NDAction = NDPush NDTYPE		--
-	|NDPop				-- 
-	|NDSwap 			--
-	|NDDSwap			--
-	|NDRotR				-- для смещения стека по кольцу
-	|NDRotL				-- -||-
-	|NDDup				--
-	|NDAdd				--
-	|NDSub				--
-	|NDMul				--
-	|DivD				-- для деления Double
-	|Div				-- целая часть деления
-	|Mod				-- остаток от деления
-	|GE				-- сравнения >:<:==:>=:<=:<>
-	|LE				--
-	|G				--
-	|L				--
-	|E				--
-	|NE				--
-	|NOT				--
-	|AND				--
-	|OR				--
-	|XOR				--
-	|NDIf [NDAction] [NDAction]	-- условие NDIf [При True] [При False]
-	|NDNewFunction NDTYPE [NDAction]-- объявление функции
-	|NDCallFunction NDTYPE		-- вызов функции
-	|NDSCallFunction		-- вызов функции с вершины стека
-	|NDExit         		-- выход из п/программы, т.е. функции
-	|NDCat
+data NDAction = NDPush NDCoords NDTYPE			--
+	|NDPop NDCoords								-- 
+	|NDSwap NDCoords							--
+	|NDDSwap NDCoords							--
+	|NDRotR NDCoords							-- для смещения стека по кольцу
+	|NDRotL NDCoords							-- -||-
+	|NDDup NDCoords								--
+	|NDAdd NDCoords								--
+	|NDSub NDCoords								--
+	|NDMul NDCoords								--
+	|DivD NDCoords								-- для деления Double
+	|Div NDCoords								-- целая часть деления
+	|Mod NDCoords								-- остаток от деления
+	|GE NDCoords								-- сравнения >:<:==:>=:<=:<>
+	|LE NDCoords								--
+	|G NDCoords									--
+	|L NDCoords									--
+	|E NDCoords									--
+	|NE NDCoords								--
+	|NOT NDCoords								--
+	|AND NDCoords								--
+	|OR NDCoords								--
+	|XOR NDCoords								--
+	|NDIf NDCoords [NDAction] [NDAction]		-- условие NDIf [При True] [При False]
+	|NDNewFunction NDCoords NDTYPE [NDAction]	-- объявление функции
+	|NDCallFunction NDCoords NDTYPE				-- вызов функции
+	|NDSCallFunction NDCoords					-- вызов функции с вершины стека
+	|NDExit NDCoords         					-- выход из п/программы, т.е. функции
+	|NDCat NDCoords								-- nyan
 -------------------------------------------------------------------------------
