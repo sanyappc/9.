61ca14df26c707928268448207ffab8c59dd4c68
Drogunov Igor <drogunov.igor@gmail.com>
2012-12-16 22:13:56 +0400

    поправил форматирование. выбросил лишнее.

--- modules/NDAction.hs
+++ modules/NDAction.hs
@@ -9,31 +9,31 @@ module NDAction where
 import NDType
 
 -------------------------------------------------------------------------------
-{- Тип Action предназначен для описания базовых функций нашего языка -}
+-- Тип Action предназначен для описания базовых функций нашего языка
 -------------------------------------------------------------------------------
-data NDAction = NDPush NDTYPE		--
-	|NDPop				-- 
-	|NDSwap 			--
-	|NDDSwap			--
-	|NDRotR				-- для смещения стека по кольцу
-	|NDRotL				-- -||-
-	|NDDup				--
-	|NDAdd				--
-	|NDSub				--
-	|NDMul				--
-	|DivD				-- для деления Double
-	|Div				-- целая часть деления
-	|Mod				-- остаток от деления
-	|GE					-- сравнения >:<:==:>=:<=:<>
-	|LE					--
-	|G					--
-	|L					--
-	|E					--
-	|NE					--
-	|NOT				--
-	|AND				--
-	|OR					--
-	|XOR				--
+data NDAction = NDPush NDTYPE
+	|NDPop					-- 
+	|NDSwap 				--
+	|NDDSwap				--
+	|NDRotR					-- для смещения стека по кольцу
+	|NDRotL					-- -||-
+	|NDDup					--
+	|NDAdd					--
+	|NDSub					--
+	|NDMul					--
+	|DivD					-- для деления Double
+	|Div					-- целая часть деления
+	|Mod					-- остаток от деления
+	|GE						-- сравнения >:<:==:>=:<=:<>
+	|LE						--
+	|G						--
+	|L						--
+	|E						--
+	|NE						--
+	|NOT					--
+	|AND					--
+	|OR						--
+	|XOR					--
 	|NDIf [NDActionPos] [NDActionPos]	-- условие NDIf [При True] [При False]
 	|NDNewFunction NDTYPE [NDActionPos]-- объявление функции
 	|NDCallFunction NDTYPE		-- вызов функции
